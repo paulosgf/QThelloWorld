6b2f6674434e17a63416cc2a4f29c667f0aa91b8
Paulo Ferraz <paulosgf.pf@gmail.com>
2025-03-23 18:00:18 -0300

    Add style formatation

--- QThelloWorld.pro
+++ QThelloWorld.pro
@@ -14,8 +14,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
-    mainwindow.h \
-    utils.h
+    mainwindow.h
 
 FORMS += \
     mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,46 +1,123 @@
 #include "mainwindow.h"
-#include <QVBoxLayout>
-#include <QComboBox>
-#include <QPushButton>
-#include <QDebug>
+#include <QPropertyAnimation>
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
-    , m_userCombo(new QComboBox(this))
-    , m_authButton(new QPushButton("Autenticar", this))
+{
+    setupUI();
+    applyStyle();
+    setupConnections();
+}
+
+void MainWindow::setupUI()
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(m_userCombo);
-    layout->addWidget(m_authButton);
+    layout->setAlignment(Qt::AlignCenter);
 
+    // ComboBox de usuários
+    m_userCombo = new QComboBox(this);
     m_userCombo->setModel(&m_usersModel);
 
-    connect(m_authButton, &QPushButton::clicked, this, &MainWindow::handleAuthentication);
+    // Botão de autenticação
+    m_authButton = new QPushButton("Autenticar", this);
+
+    // Label de status
+    m_statusLabel = new QLabel(this);
+    m_statusLabel->setAlignment(Qt::AlignCenter);
+    m_statusLabel->hide();
+
+    layout->addWidget(m_userCombo);
+    layout->addWidget(m_authButton);
+    layout->addWidget(m_statusLabel);
+}
+
+void MainWindow::applyStyle()
+{
+    // Estilo geral
+    this->setStyleSheet(R"(
+        QWidget {
+            background-color: #2d2d2d;
+            color: #ffffff;
+            font-size: 16px;
+        }
+
+        QComboBox {
+            min-width: 300px;
+            min-height: 50px;
+            padding: 10px;
+            border: 2px solid #4d4d4d;
+            border-radius: 5px;
+            selection-background-color: #404040;
+        }
+
+        QComboBox QAbstractItemView {
+            background-color: #404040;
+            selection-background-color: #505050;
+        }
+
+        QPushButton {
+            min-width: 200px;
+            min-height: 50px;
+            background-color: #606060;
+            border: 2px solid #4d4d4d;
+            border-radius: 5px;
+            padding: 10px;
+        }
+
+        QPushButton:hover {
+            background-color: #707070;
+        }
+
+        QPushButton:pressed {
+            background-color: #505050;
+        }
+
+        QLabel#statusLabel {
+            color: #ff4444;
+            font-size: 14px;
+        }
+    )");
+
+    // Estilo específico para o label de status
+    m_statusLabel->setObjectName("statusLabel");
+}
+
+void MainWindow::setupConnections()
+{
+    connect(m_authButton, &QPushButton::clicked, this, &MainWindow::authenticateUser);
     connect(&m_greeter, &QLightDM::Greeter::authenticationComplete, this, &MainWindow::handleAuthenticationResult);
 }
 
-bool MainWindow::connectToGreeter()
+void MainWindow::authenticateUser()
 {
-    return m_greeter.connectSync();
-}
-
-void MainWindow::handleAuthentication()
-{
-    const QString username = m_userCombo->currentData(QLightDM::UsersModel::NameRole).toString();
+    QString username = m_userCombo->currentData(QLightDM::UsersModel::NameRole).toString();
     m_greeter.authenticate(username);
 }
 
 void MainWindow::handleAuthenticationResult()
 {
-    if (m_greeter.isAuthenticated()) {
-        const QModelIndex sessionIndex = m_sessionsModel.index(0, 0);
-        if (sessionIndex.isValid()) {
-            const QString sessionKey = m_sessionsModel.data(sessionIndex, QLightDM::SessionsModel::KeyRole).toString();
+    QPropertyAnimation *anim = new QPropertyAnimation(m_statusLabel, "geometry");
+    anim->setDuration(500);
+    anim->setEasingCurve(QEasingCurve::OutQuad);
+
+    if(m_greeter.isAuthenticated()) {
+        m_statusLabel->setStyleSheet("color: #44ff44;");
+        m_statusLabel->setText("Autenticação bem-sucedida!");
+
+        // Iniciar sessão
+        QModelIndex sessionIndex = m_sessionsModel.index(0, 0);
+        if(sessionIndex.isValid()) {
+            QString sessionKey = m_sessionsModel.data(sessionIndex, QLightDM::SessionsModel::KeyRole).toString();
             m_greeter.startSessionSync(sessionKey);
-        } else {
-            qCritical() << "Nenhuma sessão disponível!";
         }
     } else {
-        qDebug() << "Autenticação falhou!";
+        m_statusLabel->setStyleSheet("color: #ff4444;");
+        m_statusLabel->setText("Falha na autenticação!");
     }
+
+    // Animação de fade-in
+    m_statusLabel->show();
+    anim->setStartValue(QRect(m_statusLabel->x(), m_statusLabel->y() + 20, m_statusLabel->width(), m_statusLabel->height()));
+    anim->setEndValue(m_statusLabel->geometry());
+    anim->start();
 }
